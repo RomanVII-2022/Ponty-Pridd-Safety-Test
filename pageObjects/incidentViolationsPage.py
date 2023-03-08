e3f65e8b6129fd81368359ebc03d6bb6da7b6138
Victor Mwai <vicmkinyua77@gmail.com>
2023-03-08 09:19:28 +0300

    incident violations

---
+++ pageObjects/incidentViolationsPage.py
@@ -0,0 +1,101 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+
+class IncidentTypes:
+
+
+    def __init__(self, driver, logger):
+        self.driver = driver
+        self.logger = logger
+
+
+    def tabVilotation(self):
+        try:
+            violation = self.driver.find_element(By.XPATH, "(//button[normalize-space()='Violations'])[1]")
+            violation.click()
+            self.logger.debug("**** Violations tab was clicked successfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Violations tab was not clicked ****")
+
+
+    def addViolation(self):
+        try:
+            btn = self.driver.find_element(By.XPATH, "//button[normalize-space()='Add Violation']")
+            btn.click()
+            self.logger.debug("**** Add Violation button was clicked successfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Add Violation button was not clicked ****")
+
+
+    def violationName(self, name):
+        try:
+            nameInput = self.driver.find_element(By.XPATH, "//input[@name='name']")
+            nameInput.clear()
+            nameInput.send_keys(name)
+            self.logger.debug("**** Violation name was added successfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Violation name was not added ****")
+
+
+    def violationDescription(self, description):
+        try:
+            descriptionInput = self.driver.find_element(By.XPATH, "//input[@name='description']")
+            descriptionInput.clear()
+            descriptionInput.send_keys(description)
+            self.logger.debug("**** Description was added successfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Description was not added ****")
+
+
+    def proposedAction(self, action):
+        try:
+            actionInput = self.driver.find_element(By.XPATH, "//input[@name='action']")
+            actionInput.clear()
+            action.send_keys(action)
+            self.logger.debug("**** Proposed action was entered successfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Proposed action was not entered ****")
+
+
+    def saveBtn(self):
+        try:
+            btn = self.driver.find_element(By.XPATH, "//button[normalize-space()='Save']")
+            btn.click()
+            self.logger.debug("**** Save button was clicked succcessfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Save button was not clicked ****")
+
+
+    def closeBtn(self):
+        try:
+            btn = self.driver.find_element(By.XPATH, "//button[normalize-space()='Close']")
+            btn.click()
+            self.logger.debug("**** Close button was clicked successfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Close button was not clicked ****")
+
+
+    def errormsg(self):
+        try:
+            msg = self.driver.find_element(By.XPATH, "//div[@class='errmsg']")
+            return msg.text
+        except:
+            return "No error"
+
+
+    def confirmMessage(self):
+        try:
+            confmsg = self.driver.find_element(By.XPATH, "//div[@class='confmsg']")
+            return confmsg.text
+        except:
+            return "No confirmation message"
+
+
+    def editBtn(self):
+        try:
+            btn = self.driver.find_element(By.XPATH, "//tbody/tr[1]/td[5]/button[1]")
+            btn.click()
+            self.logger.debug("**** Edit button was clicked successfully ****")
+        except:
+            self.logger.debug("**** Something went wrong. Edit button could not be found ****")
---
+++ testCases/test_006_incident_violations.py
@@ -0,0 +1,126 @@
+from utilities.readproperty import ReadConfig
+from utilities.customlogger import LogGen
+from pageObjects.incidentViolationsPage import IncidentTypes
+import time
+
+
+class Test_003_users:
+    base_url = ReadConfig.get_app_url()
+    logger = LogGen().log_gen()
+
+    def test_add_violation(self, setup):
+        self.driver = setup
+        violations = IncidentTypes(self.driver, self.logger)
+        violations.tabVilotation()
+        violations.addViolation()
+        violations.violationName("Violation")
+        violations.violationDescription("Violation Description")
+        violations.proposedAction("Exile")
+        violations.saveBtn
+        confmsg = violations.confirmMessage()
+        if confmsg == "Violation Added Succesfully":
+            assert True
+            self.logger.debug("**** Confirmation message matched the expected confirmation message ****")
+            violations.closeBtn()
+        else:
+            self.driver.save_screenshot("/home/vmwai/Documents/tests/PontySafety/screenshots/incidentviolationadd.png")
+            self.logger.debug("**** Something went wrong. Confirmation message did not match the expected confirmation message ****")
+            violations.closeBtn()
+            assert False
+    
+
+    def test_existing_violation(self, setup):
+        self.driver = setup
+        violations = IncidentTypes(self.driver, self.logger)
+        violations.addViolation()
+        violations.violationName("Violation")
+        violations.violationDescription("Violation Description")
+        violations.proposedAction("Exile")
+        violations.saveBtn
+        errmsg = violations.errormsg()
+        if errmsg == "Request failed with status code 400":
+            assert True
+            self.logger.debug("**** Error message matched the expected error message ****")
+            violations.closeBtn()
+        else:
+            self.driver.save_screenshot("/home/vmwai/Documents/tests/PontySafety/screenshots/incidentviolationexisting.png")
+            self.logger.debug("**** Something went wrong. Error message did not match the expected error message ****")
+            violations.closeBtn()
+            assert False
+
+
+    def test_noname_violation(self, setup):
+        self.driver = setup
+        violations = IncidentTypes(self.driver, self.logger)
+        violations.addViolation()
+        violations.violationDescription("Violation Description")
+        violations.proposedAction("Exile")
+        violations.saveBtn
+        errmsg = violations.errormsg()
+        if errmsg == "Name cannot be Empty":
+            assert True
+            self.logger.debug("**** Error message matched the expected error message ****")
+            violations.closeBtn()
+        else:
+            self.driver.save_screenshot("/home/vmwai/Documents/tests/PontySafety/screenshots/incidentviolationnoname.png")
+            self.logger.debug("**** Something went wrong. Error message did not match the expected error message ****")
+            violations.closeBtn()
+            assert False
+
+
+    def test_nodescription_violation(self, setup):
+        self.driver = setup
+        violations = IncidentTypes(self.driver, self.logger)
+        violations.addViolation()
+        violations.violationName("Violation")
+        violations.proposedAction("Exile")
+        violations.saveBtn
+        errmsg = violations.errormsg()
+        if errmsg == "Description cannot be Empty":
+            assert True
+            self.logger.debug("**** Error message matched the expected error message ****")
+            violations.closeBtn()
+        else:
+            self.driver.save_screenshot("/home/vmwai/Documents/tests/PontySafety/screenshots/incidentviolationnodescription.png")
+            self.logger.debug("**** Something went wrong. Error message did not match the expected error message ****")
+            violations.closeBtn()
+            assert False
+
+
+    def test_noaction_violation(self, setup):
+        self.driver = setup
+        violations = IncidentTypes(self.driver, self.logger)
+        violations.addViolation()
+        violations.violationName("Violation")
+        violations.violationDescription("Violation Description")
+        violations.saveBtn
+        errmsg = violations.errormsg()
+        if errmsg == "Action cannot be Empty":
+            assert True
+            self.logger.debug("**** Error message matched the expected error message ****")
+            violations.closeBtn()
+        else:
+            self.driver.save_screenshot("/home/vmwai/Documents/tests/PontySafety/screenshots/incidentviolationnoaction.png")
+            self.logger.debug("**** Something went wrong. Error message did not match the expected error message ****")
+            violations.closeBtn()
+            assert False
+
+
+    def test_edit_violation(self, setup):
+        self.driver = setup
+        violations = IncidentTypes(self.driver, self.logger)
+        violations.editBtn()
+        violations.saveBtn
+        confmsg = violations.confirmMessage()
+        if confmsg == "Violation Edited Succesfully":
+            assert True
+            self.logger.debug("**** Confirmation message matched the expected confirmation message ****")
+            violations.closeBtn()
+        else:
+            self.driver.save_screenshot("/home/vmwai/Documents/tests/PontySafety/screenshots/incidentviolationadd.png")
+            self.logger.debug("**** Something went wrong. Confirmation message did not match the expected confirmation message ****")
+            violations.closeBtn()
+            assert False
+
+
+    
